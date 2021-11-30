032d4c72556f6082f03d19217e194154cf55ec72
batyadmx <andronovaleksandrptg@yandex.ru>
2021-11-30 22:47:09 +0500

    Добавил метод получения сообщений

--- Controllers/ChatsController.cs
+++ Controllers/ChatsController.cs
@@ -3,11 +3,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VideoMessenger.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace VideoMessenger.Controllers
 {
     public class ChatsController : Controller
     {
+        private ApplicationContext db;
+
+        public ChatsController(ApplicationContext context)
+        {
+            db = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,7 +25,9 @@ namespace VideoMessenger.Controllers
         [Route("chats/{id:int}/messages")]
         public async Task<IActionResult> GetChatMessages(int id)
         {
-            return Content(id.ToString());
+            var messages = db.Messages.Where(m => m.Id == id).ToArray();
+            var json = JsonSerializer.Serialize(messages);
+            return Ok();
         }
     }
 }
