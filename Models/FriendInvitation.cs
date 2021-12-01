94b7eb5c35d562a297f34ca9249f09b1bf9e2d36
Maxim <atm.tsvetk@gmail.com>
2021-12-01 19:27:04 +0500

    Создал таблицу FriendInvitation

---
+++ Models/FriendInvitation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoMessenger.Models
+{
+    public class FriendInvitation
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public User Sender { get; set; }
+        public int RecipientId { get; set; }
+        public User Recipient { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
