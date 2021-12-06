8ed245f44e5d7950d983b47ab9e4fb3f7b793765
Maxim <atm.tsvetk@gmail.com>
2021-12-06 18:16:32 +0500

    Добавил методы для работы с Друзьями

---
+++ ViewModels/FriendData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoMessenger.ViewModels
+{
+    public class FriendData
+    {
+        [Required]
+        public string User { get; set; }
+        [Required]
+        public string Friend { get; set; }
+    }
+}
