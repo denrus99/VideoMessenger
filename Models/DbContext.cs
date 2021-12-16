dd1531973c984a808bd69110ee54d69560f429fa
Maxim <atm.tsvetk@gmail.com>
2021-12-16 15:21:10 +0500

    Создал миграцию на создание базы данных

--- Models/DbContext.cs
+++ Models/DbContext.cs
@@ -64,9 +64,9 @@ namespace VideoMessenger.Models
                 );
 
             builder.Entity<User>().HasData(
-                new User() { Id = -1, Username = "Vasya", Login = "Pupok", EmailAddress = "vasia322@bbqqkk.com", Password = "ahc4ahlv4lt", PhoneNumber = "+78222345678" },
-                new User() { Id = -2,Username = "Igor", Login = "Uxo", EmailAddress = "igor228@bbqqkk.com", Password = "aklovfaoper", PhoneNumber = "+79221234564" },
-                new User() { Id = -3,Username = "Masha", Login = "Davalka", EmailAddress = "openheart@bbqqkk.com", Password = "12345", PhoneNumber = "+78921234567" });
+                new User() { Id = -1, Username = "Vasya", Login = "Pupok", EmailAddress = "vasya322@ya.ru", Password = "ahc4ahlv4lt", PhoneNumber = "+78222345678" },
+                new User() { Id = -2, Username = "Igor", Login = "Uxo", EmailAddress = "igor2005@ya.ru", Password = "aklovfaoper", PhoneNumber = "+79221234564" },
+                new User() { Id = -3, Username = "Masha", Login = "Poland", EmailAddress = "sodeep@ya.ru", Password = "12345", PhoneNumber = "+78921234567" });
 
             builder.Entity<Friends>().HasData(
                 new Friends(-1, -2),
@@ -76,21 +76,31 @@ namespace VideoMessenger.Models
                 );
 
             builder.Entity<Chat>().HasData(
-                new Chat("RI-390003", new DateTime(2021, 12, 1, 20, 16, 11, 191, DateTimeKind.Local).AddTicks(4330), -1),
-                new Chat("Gamers Room", new DateTime(2021, 12, 2, 20, 16, 11, 191, DateTimeKind.Local).AddTicks(4330), -2)
+                new Chat("RI-390003", new DateTime(2021, 12, 16, 13, 16, 0, 0, DateTimeKind.Local), -1),
+                new Chat("Gamers Room", new DateTime(2021, 12, 2, 20, 16, 11, 191, DateTimeKind.Local), -2)
                 );
 
             builder.Entity<Message>().HasData(
-                new Message() { Id = -1, SenderId = -1, ChatId= -1, Data="Hello, World!", CreationDate= new DateTime(2021, 12, 1, 20, 16, 11, 191, DateTimeKind.Local).AddTicks(4330), IsReaded =false },
-                new Message() { Id = -2, SenderId = -2, ChatId= -2, Data="Good bye, World!", CreationDate= new DateTime(2021, 11, 2, 21, 16, 11, 191, DateTimeKind.Local).AddTicks(4330), IsReaded =true },
-                new Message() { Id = -3, SenderId = -3, ChatId= -2, Data="My life be like, uuuuaaaa", CreationDate= new DateTime(2021, 10, 3, 20, 16, 11, 191, DateTimeKind.Local).AddTicks(4330), IsReaded =false }
+                new Message() { Id = -1, SenderId = -1, ChatId= -1, Data="Привет, как у вас дела?", CreationDate= new DateTime(2021, 12, 16, 13, 16, 10, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -2, SenderId = -2, ChatId= -1, Data="Привет, нормально", CreationDate= new DateTime(2021, 12, 16, 13, 16, 12, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -3, SenderId = -3, ChatId= -1, Data="Отлично! Как у тебя?", CreationDate= new DateTime(2021, 12, 16, 13, 16, 14, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -4, SenderId = -1, ChatId= -1, Data="Могло быть и лучше", CreationDate= new DateTime(2021, 12, 16, 13, 16, 16, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -5, SenderId = -2, ChatId= -1, Data="Что случилось?", CreationDate= new DateTime(2021, 12, 16, 13, 16, 20, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -6, SenderId = -1, ChatId= -1, Data="Получил 4 по алгебре", CreationDate= new DateTime(2021, 12, 16, 13, 16, 25, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -7, SenderId = -3, ChatId= -1, Data="У меня вообще тройка, не стоит отчаиваться", CreationDate= new DateTime(2021, 12, 16, 13, 16, 30, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -8, SenderId = -2, ChatId= -1, Data="Нормально все будет", CreationDate= new DateTime(2021, 12, 16, 13, 16, 35, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -9, SenderId = -3, ChatId= -1, Data="Приезжайте ко мне, будем в шашки играть", CreationDate= new DateTime(2021, 12, 16, 13, 16, 36, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -10, SenderId = -1, ChatId= -1, Data="Выезжаю...", CreationDate= new DateTime(2021, 12, 1, 13, 16, 38, 191, DateTimeKind.Local), IsReaded = false },
+                new Message() { Id = -11, SenderId = -2, ChatId= -2, Data="Good bye, World!", CreationDate= new DateTime(2021, 12, 2, 20, 16, 11, 191, DateTimeKind.Local), IsReaded = true },
+                new Message() { Id = -12, SenderId = -3, ChatId= -2, Data="My life be like, uuuuaaaa", CreationDate= new DateTime(2021, 12, 2, 21, 16, 11, 191, DateTimeKind.Local), IsReaded = false }
                 );
+
             builder.Entity<ChatParticipant>().HasData(
                 new ChatParticipant(-1, -1, 1),
                 new ChatParticipant(-2, -1, 2),
+                new ChatParticipant(-3, -1, 3),
                 new ChatParticipant(-2, -2, 1),
-                new ChatParticipant(-3, -2, 1),
-                new ChatParticipant(-3, -1, 2)
+                new ChatParticipant(-1, -2, 2)
                 );
         }
     }
