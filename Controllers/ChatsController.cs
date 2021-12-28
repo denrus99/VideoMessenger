a0393b50f75a27ec6164670bd3003c4f476085dc
Maxim <atm.tsvetk@gmail.com>
2021-12-28 22:49:38 +0500

    Изменил метод получения инвайтов в чаты

--- Controllers/ChatsController.cs
+++ Controllers/ChatsController.cs
@@ -30,7 +30,13 @@ namespace VideoMessenger.Controllers
         /* Метод получения инвайтов в чат */
         public async Task<IActionResult> GetChatInvitations(string login)
         {
-            var invitations = await db.ChatInvitations.Include(x => x.Chat).Where(x => x.Recipient.Login == login).ToArrayAsync();
+            var invitations = await db.ChatInvitations
+                .Include(x => x.Chat)
+                .Include(x => x.Recipient)
+                .Include(x => x.Sender)
+                .Where(x => x.Recipient.Login == login)
+                .Select(x => new InvitationInfo(x))
+                .ToArrayAsync();
             var json = JsonSerializer.Serialize(invitations);
             return Ok(json);
         }
